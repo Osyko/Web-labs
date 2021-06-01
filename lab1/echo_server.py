10e58921d349b2c3a001ae18a86881928c6367c1
Osyko <osykodarya@gmail.com>
2021-06-01 11:54:50 +0300

    lab1

--- lab1/echo_server.py
+++ lab1/echo_server.py
@@ -1,39 +1,3 @@
-'''import sys
-import socket
-import json
-from threading import Thread
-
-def square_area(n):
-    n = pow(int(n), 2)
-    return n
-
-def server(c: socket, client_address):
-    print(sys.stderr, 'waiting for a connection')
-    size = c.getsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF)
-    data = c.recv(size)
-    data = json.loads(data.decode("utf-8"))
-    print(f'{client_address} >> Area = {data["request"]} cm')
-    print(sys.stderr, 'received "%s"' % data)
-    print(sys.stderr, 'sending data back to the client')
-    data = json.dumps({"result": square_area(data['request'])}).encode("utf-8")
-    c.sendall(data)
-    c.close()
-
-def main():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = ('localhost', 1025)
-    sock.bind(server_address)
-    sock.listen(15)
-    while True:
-        connection, client_address = sock.accept()
-        client = Thread(target=server, args=(connection,))
-        print(sys.stderr, 'connection from', client_address)
-        client.start()
-
-if __name__ == '__main__':
-    main()
-'''
-
 from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_RCVBUF
 from threading import Thread
 import json
